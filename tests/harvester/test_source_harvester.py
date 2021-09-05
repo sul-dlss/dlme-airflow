95af77feb8e8be18fbe1b9a1efd90c7bba1bbf2b
Aaron Collier <aaron.collier@stanford.edu>
2021-09-05 09:53:21 -0700

    Update existing tests after refactor

--- tests/harvester/test_source_harvester.py
+++ tests/harvester/test_source_harvester.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pytest
 import pandas as pd
 
-from harvester.source_harvester import data_source_harvester, get_existing_df
+from harvester.source_harvester import data_source_harvester
 
 
 @pytest.fixture
@@ -20,10 +20,4 @@ def test_source_harvester():
 
 def test_provider_not_found():
     with pytest.raises(AttributeError, match=r"bad_provider"):
-        data_source_harvester("bad_provider")
-
-
-def test_get_existing_df(csv_fixture):
-    mock_directory = Path()
-    result = get_existing_df("csv", mock_directory)
-    assert isinstance(result, pd.DataFrame)
+        data_source_harvester(provider="bad_provider")
