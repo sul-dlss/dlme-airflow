03b07b10f131d644ea9e73225640b7682f592e63
Jacob Hill <jacobhill.mail@gmail.com>
2021-10-13 16:28:31 -0400

    fix pylint error

--- tests/drivers/test_iiif_json.py
+++ tests/drivers/test_iiif_json.py
@@ -32,6 +32,7 @@ class MockIIIFManifestResponse:
                 {"canvases": [{"images": [{"resource": {"format": "image/jpeg"}}]}]}
             ],
         }
+        
     @staticmethod
     def text():
         return {
