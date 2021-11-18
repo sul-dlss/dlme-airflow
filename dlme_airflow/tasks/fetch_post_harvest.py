5983e2dd5050d0f523fb5d6a37f2e8eb2583c02f
Aaron Collier <aaron.collier@stanford.edu>
2021-11-18 10:44:06 -0800

    Move post-harvest field into metadata and make script executable

--- dlme_airflow/tasks/fetch_post_harvest.py
+++ dlme_airflow/tasks/fetch_post_harvest.py
@@ -4,6 +4,6 @@ from utils.catalog import catalog_for_provider
 
 def fetch_post_harvest(provider):
     source = catalog_for_provider(provider)
-    post_harvest_script = f"{source.get('post_harvest')}"
+    post_harvest_script = f"{source.metadata.get('post_harvest')}"
 
     subprocess.run(post_harvest_script)
