dc912e72befc3fd4a4394c55e594989b77e3e6e0
Jacob Hill <jacobhill.mail@gmail.com>
2021-12-07 14:46:32 -0500

    fix style issues

--- dlme_airflow/tasks/fetch_post_harvest.py
+++ dlme_airflow/tasks/fetch_post_harvest.py
@@ -1,12 +1,9 @@
-import importlib
-import subprocess
 from utils.catalog import catalog_for_provider
+
 
 def fetch_post_harvest(provider):
     source = catalog_for_provider(provider)
     post_harvest_script = f"{source.metadata.get('post_harvest')}"
-
-    # importlib.import_module(post_harvest_script)
 
     with open(post_harvest_script, "rb") as source_file:
         code = compile(source_file.read(), post_harvest_script, "exec")
