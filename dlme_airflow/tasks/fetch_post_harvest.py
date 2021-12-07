d8097e31331dc325063e99061fc8ed62014d82c1
Jacob Hill <jacobhill.mail@gmail.com>
2021-12-07 11:46:37 -0500

    revise post harvest

--- dlme_airflow/tasks/fetch_post_harvest.py
+++ dlme_airflow/tasks/fetch_post_harvest.py
@@ -1,9 +1,13 @@
+import importlib
 import subprocess
 from utils.catalog import catalog_for_provider
-
 
 def fetch_post_harvest(provider):
     source = catalog_for_provider(provider)
     post_harvest_script = f"{source.metadata.get('post_harvest')}"
 
-    subprocess.run(post_harvest_script)
+    # importlib.import_module(post_harvest_script)
+
+    with open(post_harvest_script, "rb") as source_file:
+        code = compile(source_file.read(), post_harvest_script, "exec")
+    exec(code)
--- dlme_airflow/utils/yale_babylon_remove_non_relevant_records.py
+++ dlme_airflow/utils/yale_babylon_remove_non_relevant_records.py
@@ -1,31 +1,31 @@
+#/bin/python
 import pandas as pd
 import yaml
 
-def remove_yale_non_relevant():
-    # Objects from these countries will be suppressed
-    NON_RELEVANT_COUNTRIES = ['Canada',
-                              'China',
-                              'Ecuador',
-                              'Guatemala',
-                              'Greece',
-                              'Honduras',
-                              'Italy',
-                              'Korea',
-                              'Malaysia',
-                              'Mexico',
-                              'Peru',
-                              'South Africa',
-                              'USA']
+# Objects from these countries will be suppressed
+NON_RELEVANT_COUNTRIES = ['Canada',
+                          'China',
+                          'Ecuador',
+                          'Guatemala',
+                          'Greece',
+                          'Honduras',
+                          'Italy',
+                          'Korea',
+                          'Malaysia',
+                          'Mexico',
+                          'Peru',
+                          'South Africa',
+                          'USA']
 
-    # Fetch working directory path from catalog and read file into Pandas dataframe
-    with open('../catalogs/catalog.yaml', 'r') as stream:
-        catalog = yaml.safe_load(stream)
-        path = catalog['sources']['yale_babylonian']['metadata']\
-        ['working_directory']
+# Fetch working directory path from catalog and read file into Pandas dataframe
+with open('../catalogs/catalog.yaml', 'r') as stream:
+    catalog = yaml.safe_load(stream)
+    path = catalog['sources']['yale_babylonian']['metadata']\
+    ['working_directory']
 
-    df = pd.read_csv(path)
+df = pd.read_csv(f"{path}/data.csv")
 
-    # Filter out non relevant records and over write the csv
-    df = df[~df['geographic_country'].isin(NON_RELEVANT_COUNTRIES)]
+# Filter out non relevant records and over write the csv
+df = df[~df['geographic_country'].isin(NON_RELEVANT_COUNTRIES)]
 
-    df.to_csv(path)
+df.to_csv(f"{path}/data.csv")
