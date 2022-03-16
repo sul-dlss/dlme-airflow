2ad8dc93bd1da3e028fffa63e14c8aae5518e1e3
Aaron Collier <aaron.collier@stanford.edu>
2022-03-15 21:41:02 -0700

    Use correct path to catalog files

--- dlme_airflow/utils/driver_tag.py
+++ dlme_airflow/utils/driver_tag.py
@@ -7,15 +7,13 @@ def fetch_driver(provider):
     @param -- provider The registered provider name
     """
     drivers = []
-    with open("../catalogs/catalog.yaml", "r") as stream:
+    with open("/opt/airflow/catalogs/catalog.yaml", "r") as stream:
         catalog = yaml.safe_load(stream)
         if (
             catalog["sources"][provider].get("driver")
             == "intake.catalog.local.YAMLFileCatalog"
         ):
-            path = (
-                catalog["sources"][provider]["args"].get("path").replace("/opt", "..")
-            )
+            path = catalog["sources"][provider]["args"].get("path")
             with open(path, "r") as collection_stream:
                 collection_catalog = yaml.safe_load(collection_stream)
                 for key in collection_catalog["sources"].keys():
