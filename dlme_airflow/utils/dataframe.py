c0b610d464e17b99ad4da8ace8658bc3a6d4cedb
Aaron Collier <aaron.collier@stanford.edu>
2022-03-18 13:46:43 -0700

    Write dataframe to an s3 object instead of local storage

--- dlme_airflow/utils/dataframe.py
+++ dlme_airflow/utils/dataframe.py
@@ -1,3 +1,5 @@
+import boto3
+import logging
 import os
 import pandas as pd
 
@@ -20,7 +22,10 @@ def dataframe_from_file(driver: str, data_file_path: str) -> pd.DataFrame:
 # TODO: An Error is thrown on line 22 if working_directory is not found in
 #       the metadata. Need to handle this error.
 def dataframe_to_file(dataframe, provider, collection):
-    root_dir = os.path.dirname(os.path.abspath("metadata"))
+    session = boto3.Session()
+    s3 = session.resource("s3")
+
+    # root_dir = os.path.dirname(os.path.abspath("metadata"))
     if collection:
         default_data_path = f"{provider}/{collection}"
     else:
@@ -28,12 +33,17 @@ def dataframe_to_file(dataframe, provider, collection):
 
     data_path = dataframe.metadata.get("data_path", default_data_path)
 
-    working_csv = os.path.join(root_dir, "working", data_path, "data.csv")
-    working_directory = os.path.join(root_dir, "working", data_path)
-    os.makedirs(working_directory, exist_ok=True)
+    # working_csv = os.path.join(root_dir, "working", data_path, "data.csv")
+    s3_object = s3.Object("dlme_metadata", os.path.join(data_path, "data.csv"))
+
+    # working_directory = os.path.join(root_dir, "working", data_path)
+    # os.makedirs(working_directory, exist_ok=True)
 
     unique_id = (
         dataframe.metadata.get("fields").get("id").get("name_in_dataframe", "id")
     )
     source_df = dataframe.read().drop_duplicates(subset=[unique_id], keep="first")
-    source_df.to_csv(working_csv, index=False)
+    # source_df.to_csv(working_csv, index=False)
+    result = s3_object.put(Body=source_df.to_csv)
+    res = result.get("ResponseMetadata")
+    logging.info(f"S3 Status Response: {res.get('HTTPStatusCode')}")
