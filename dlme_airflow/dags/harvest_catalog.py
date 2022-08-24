78457dfd8953c040bdffa7a0ca50a0d0f48426e3
Johnathan Martin <john.martin@stanford.edu>
2022-08-24 11:03:43 -0700

    refactor DAG instantiation for more testability, add cycle checking test
    also, explicitly add the apache-airflow-providers-amazon dependency and rename the call to an older version's deprecated method (ECSOperator to EcsRunTaskOperator).

--- dlme_airflow/dags/harvest_catalog.py
+++ dlme_airflow/dags/harvest_catalog.py
@@ -1,40 +1,4 @@
-import intake
-import logging
+from services.harvest_dag_generator import create_provider_dags, register_drivers
 
-from datetime import timedelta
-
-# Operators and utils required from airflow
-from airflow.models import Variable
-
-from drivers.iiif_json import IiifJsonSource
-from drivers.feed import FeedSource
-from drivers.oai_xml import OaiXmlSource
-from drivers.xml import XmlSource
-from drivers.sequential_csv import SequentialCsvSource
-from utils.catalog import fetch_catalog
-from services.harvest_dag_generator import create_dag
-from models.provider import Provider
-
-intake.source.register_driver("iiif_json", IiifJsonSource)
-intake.source.register_driver("oai_xml", OaiXmlSource)
-intake.source.register_driver("feed", FeedSource)
-intake.source.register_driver("xml", XmlSource)
-intake.source.register_driver("sequential_csv", SequentialCsvSource)
-
-# These args will get passed on to each operator
-# You can override them on a per-task basis during operator initialization
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "email": [Variable.get("data_manager_email")],
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": 0,
-    "retry_delay": timedelta(seconds=60),
-    "catchup": False,
-}
-
-for provider in iter(list(fetch_catalog())):
-    current_provider = Provider(provider)
-    logging.info(f"Creating DAG for {current_provider.name}")
-    globals()[provider] = create_dag(current_provider, default_args)
+register_drivers()
+create_provider_dags()
---
+++ tests/services/test_harvest_dag_generator.py
@@ -0,0 +1,29 @@
+import pytest
+
+from airflow import models
+from airflow.utils.dag_cycle_tester import check_cycle
+
+from dlme_airflow.services.harvest_dag_generator import (
+    create_provider_dags,
+    register_drivers,
+    harvest_dags,
+)
+from utils.catalog import fetch_catalog
+
+
+@pytest.fixture
+def mock_variable(monkeypatch):
+    def mock_get(key):
+        if key == "data_manager_email":
+            return "datamanager@institution.edu"
+
+    monkeypatch.setattr(models.Variable, "get", mock_get)
+
+
+def test_create_provider_dags(mock_variable):
+    register_drivers()
+    create_provider_dags()
+    assert list(harvest_dags().keys()) == list(fetch_catalog())
+    for provider in iter(list(fetch_catalog())):
+        dag = harvest_dags()[provider]
+        check_cycle(dag)
