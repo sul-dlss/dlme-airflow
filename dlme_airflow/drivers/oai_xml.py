2ec04a5665e88937c94e47614249d757e70ee1ac
Ed Summers <ehs@pobox.com>
2022-09-02 12:57:25 -0400

    Process MODS in OAI-PMH responses
    Add the ability to not blow up when encountering the MODS XML namespace
    when processing OAI-PMH responses with MODS, like what you get when you
    request "mods_no_ocr" metadataPrefix from QNL. The hierarchical MODS
    data is flattened using the element hierarchy as a column header: the
    element tag names are strung together with "_" between them.
    
    There is also a new test of the OaiXmlSource which ensures that the
    oai_dc and mods record metadata are being processed into a pandas
    DataFrame correctly.
    
    Unfortunately the QNL harvest operation doesn't succeed because of
    intermittent errors that QNL's OAI server is throwing (perhaps due to
    load?). Since that's not related to processing MODS in OAI responses
    I've ticketed that separately as #200.
    
    Closes #175

--- dlme_airflow/drivers/oai_xml.py
+++ dlme_airflow/drivers/oai_xml.py
@@ -1,8 +1,10 @@
+import re
 import logging
 import intake
 import pandas as pd
-from sickle import Sickle
+
 from lxml import etree
+from sickle import Sickle
 
 
 class OaiXmlSource(intake.source.base.DataSource):
@@ -59,20 +61,28 @@ class OaiXmlSource(intake.source.base.DataSource):
                         output[field].append(data.text.strip())
         return output
 
-    def uri2label(self, value: str, nsmap: dict):
-        for key in list(nsmap.keys()):
-            if nsmap[key] in value:
-                return value.replace(nsmap[key], "").replace("{}", "")
+    def _get_tag(self, el):
+        """Return the element name, after removing namespace and lowercasing."""
+        return re.sub(r"{.+}", "", el.tag).lower()
 
     # TODO: Discuss if this output shoould be an array (line 63) or a string
     def _from_metadata(self, manifest: etree) -> dict:
         output = {}
-        NS = {"oai_dc": "http://www.openarchives.org/OAI/2.0/oai_dc/"}
-        oai_block = manifest.xpath("//oai_dc:dc", namespaces=NS)[
-            0
-        ]  # we want the first result
+
+        NS = {
+            "oai_dc": "http://www.openarchives.org/OAI/2.0/oai_dc/",
+            "mods": "http://www.loc.gov/mods/v3",
+        }
+
+        if self.metadata_prefix == "oai_dc":
+            oai_block = manifest.xpath("//oai_dc:dc", namespaces=NS)[0]
+        elif self.metadata_prefix == "mods" or self.metadata_prefix == "mods_no_ocr":
+            oai_block = manifest.xpath("//mods:mods", namespaces=NS)[0]
+        else:
+            raise Exception(f"Unknown metadata prefix {self.metadata_prefix}")
+
         for metadata in oai_block.getchildren():
-            tag = self.uri2label(metadata.tag, metadata.nsmap)
+            tag = self._get_tag(metadata)
             if tag in output:
                 if isinstance(output[tag], str):
                     if metadata.text is not None:
@@ -81,8 +91,11 @@ class OaiXmlSource(intake.source.base.DataSource):
                         output[tag] = [output[tag]]
                 else:
                     output[tag].append(metadata.text.strip())
-            else:
+            elif metadata.text is not None:
                 output[tag] = metadata.text.strip()
+            # if the element has child elements add them too
+            elif len(metadata) > 0:
+                output.update(self._flatten_tree(metadata))
 
         return output
 
@@ -107,6 +120,44 @@ class OaiXmlSource(intake.source.base.DataSource):
             npartitions=1,
             extra_metadata={},
         )
+
+    def _flatten_tree(self, el, metadata=None, prefix=None):
+        """This will flatten an element and its children into a dictionary using
+        the element path as a dictionary key:
+
+        <a>
+          <b>x</b>
+          <c>y</c>
+          <d>
+            <e>z</e>
+          </d>
+        </a>
+
+        will be converted to:
+
+        {
+          "a_b": "x",
+          "a_c": "y"
+          "a_d_e": "z"
+        }
+
+        """
+        if metadata is None:
+            metadata = {}
+
+        # allow parent elements to accumulate, e.g. parent_child1_child2
+        key = self._get_tag(el)
+        if prefix is not None:
+            key = f"{prefix}_{key}"
+
+        if len(el) == 0 and el.text:
+            metadata[key] = el.text
+        else:
+            for child in el:
+                # recursive call to add child elements using key as a prefix
+                self._flatten_tree(child, metadata, key)
+
+        return metadata
 
     def read(self):
         self._load_metadata()
