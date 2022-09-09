42cf8e39c2de1d3c3c7af3cc748f383d8e19bba9
jtim <jtim@li-dl-7200-43bd.lan>
2022-09-09 12:13:44 -0400

    qnl post harvest task, fixes #199

---
+++ dlme_airflow/utils/qnl_merge_records.py
@@ -0,0 +1,16 @@
+# /bin/python
+import os
+import pandas as pd
+
+
+def merge_records(**kwargs):
+    coll = kwargs["collection"]
+    root_dir = os.path.dirname(os.path.abspath("metadata"))
+    data_path = coll.data_path()
+    working_csv = os.path.join(root_dir, "working", data_path, "data.csv")
+    df = pd.read_csv(working_csv)
+
+    # merge rows with the same shelf locator
+    df_merged = df.groupby('location_shelfLocator').agg(lambda x: x.tolist())
+
+    df_merged.to_csv(working_csv)
