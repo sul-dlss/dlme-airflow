53897c2174c1656389038141aedd42533f784421
Ed Summers <ehs@pobox.com>
2022-09-14 18:18:48 -0400

    Generic JSON driver
    Adds a new driver JsonSource similar to the XmlSource which you can
    configure in the intake catalog metadata with a `record_selector` to
    determine where in the JSON the records are found, and then a set of
    `fields` which define what DataFrame column name to use, and a `path`,
    which is a [JMESPath](https://jmespath.org/) to determine where in
    the record to find the value.
    
    For example if you had an intake catalog that contained:
    
    ```yaml
    driver: custom_json
    args:
      collection_url: https://www.loc.gov/collections/persian-language-rare-materials/?c=100&fo=json
    metadata:
      record_selector: "content.results"
      fields:
        id:
          path: "id",
        title:
          path: "title"
        thumbnail:
          path: "resources[0].image"
    ```
    
    and some JSON that looks like:
    
    ```json
    {
      "content": {
        "results": [
          {
            "id": "http://www.loc.gov/item/2017498321/",
            "title": "[Majmuʻah, or, Collection]",
            "resources": [
              "image": "https://tile.loc.gov/image-services/iiif/service:amed:plmp:m154:0334/full/pct:6.25/0/default.jpg"
            ]
          },
          {
            "id": "http://www.loc.gov/item/00313408/",
            "title": "Sakīnat al-fuz̤alāʼ mawsūm bi-ism-i tārīkhī-i Bahār-i Afghānī",
            "resources": [
              "image": "https://tile.loc.gov/image-services/iiif/service:amed:amedpllc:00406537456:0055/full/pct:6.25/0/default.jpg"
            ]
          }
        ]
      }
    }
    ```
    
    You would end up with a CSV that looks like:
    
    ```csv
    id,title,thumbnail
    "http://www.loc.gov/item/2017498321/","[Majmuʻah, or, Collection]","https://tile.loc.gov/image-services/iiif/service:amed:plmp:m154:0334/full/pct:6.25/0/default.jpg"
    "http://www.loc.gov/item/00313408/","Sakīnat al-fuz̤alāʼ mawsūm bi-ism-i tārīkhī-i Bahār-i Afghānī","https://tile.loc.gov/image-services/iiif/service:amed:amedpllc:00406537456:0055/full/pct:6.25/0/default.jpg"
    ```
    
    Closes #51

--- dlme_airflow/drivers/__init__.py
+++ dlme_airflow/drivers/__init__.py
@@ -4,6 +4,7 @@ from dlme_airflow.drivers.iiif_json import IiifJsonSource
 from dlme_airflow.drivers.oai_xml import OaiXmlSource
 from dlme_airflow.drivers.xml import XmlSource
 from dlme_airflow.drivers.sequential_csv import SequentialCsvSource
+from dlme_airflow.drivers.json import JsonSource
 
 
 def register_drivers():
@@ -11,3 +12,4 @@ def register_drivers():
     intake.source.register_driver("oai_xml", OaiXmlSource)
     intake.source.register_driver("xml", XmlSource)
     intake.source.register_driver("sequential_csv", SequentialCsvSource)
+    intake.source.register_driver("custom_json", JsonSource)
