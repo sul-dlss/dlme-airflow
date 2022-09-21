83749d8776ba88feeadb628866b144939fd7bfe7
Aaron Collier <aaron.collier@stanford.edu>
2022-09-21 14:54:30 -0700

    Refactor models for easier testing instantiation

--- dlme_airflow/models/provider.py
+++ dlme_airflow/models/provider.py
@@ -5,8 +5,22 @@ from dlme_airflow.models.collection import Collection
 class Provider(object):
     def __init__(self, catalog):
         self.name = catalog
-        self.catalog = catalog_for_provider(catalog)
-        self.collections = self.__collections_for()
+
+    @property
+    def catalog(self):
+        return catalog_for_provider(self.name)
+
+    @property
+    def collections(self):
+        _collections = []
+        try:
+            provider_collections = iter(list(self.catalog))
+            for provider_collection in provider_collections:
+                _collections.append(Collection(self, provider_collection))
+        except TypeError as err:
+            print(f"ERROR Parsing collections for provider {self.name}: {err}")
+
+        return _collections
 
     def get_collection(self, collection_name):
         for coll in self.collections:
@@ -19,14 +33,3 @@ class Provider(object):
 
     def data_path(self):
         return self.catalog.metadata.get("data_path", self.name)
-
-    def __collections_for(self):
-        collections = []
-        try:
-            provider_collections = iter(list(self.catalog))
-            for provider_collection in provider_collections:
-                collections.append(Collection(self, provider_collection))
-        except TypeError as err:
-            print(f"ERROR Parsing collections for provider {self.name}: {err}")
-
-        return collections
