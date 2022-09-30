50571ae49b0d4a8c200692bc52dd516ffad6ff26
Johnathan Martin <john.martin@stanford.edu>
2022-09-30 02:00:51 -0700

    clean up test coverage and comments for source_harvester.py
    add pytest-mock as dev dependency to support new test coverage

--- dlme_airflow/harvester/source_harvester.py
+++ dlme_airflow/harvester/source_harvester.py
@@ -5,15 +5,13 @@ PROVIDER = "provider"
 
 
 def data_source_harvester(task_instance, **kwargs):
-    """Intake source harvester, takes a provider (for nested catalog use the catalog
-    name.source, i.e. bodleian.arabic), generates a Pandas DataFrame, runs
-    validations, and saves a copy of the DataFrame.
-
-    @param -- provider
+    """Intake source harvester, takes a Collection object, generates
+    a Pandas DataFrame from a harvest of it, and saves a copy of the
+    DataFrame.  Also, pushes some basic stats about the dataframe for
+    use in e.g. downstream validation of transform output.
     """
 
     collection = kwargs.get("collection")
-    # dataframe_to_file(source, collection.provider.name, collection)
     df_and_csv = dataframe_to_file(collection)
     df = df_and_csv["source_df"]
     working_csv = df_and_csv["working_csv"]
