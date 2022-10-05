4d90fd5bc15594dc39074e84c79ac63df621f86b
jacobthill <jacobhill.mail@gmail.com>
2022-10-05 16:05:13 -0400

    fix linting issues

--- bin/mapping/get_input_fields.py
+++ bin/mapping/get_input_fields.py
@@ -18,14 +18,14 @@ def main():
     mapped_columns = []
     files = glob.glob(f"{os.getcwd()}/working/{args.directory[0]}/*/*.csv")
     if args.d:
-        for f in files:
-            df = pd.read_csv(f)
+        for file in files:
+            df = pd.read_csv(file)
             [column_names.append(c) for c in (df.columns)]
         with open(f"{os.getcwd()}/../dlme-transform/traject_configs/{args.d}.rb", 'r') as traject_config:
             lines = traject_config.readlines()
-            for l in lines:
-                if 'column' in l:
-                    mapped_columns.append(l.split("column('")[-1].split("')")[0])
+            for line in lines:
+                if 'column' in line:
+                    mapped_columns.append(line.split("column('")[-1].split("')")[0])
             if args.r:
                 for i in sorted(set(mapped_columns).difference(set(column_names))):
                     print(i)
@@ -34,13 +34,13 @@ def main():
                     print(i)
     else:
         if args.c:
-            for f in files:
-                df = pd.read_csv(f)
+            for file in files:
+                df = pd.read_csv(file)
                 if args.c in list(df.columns):
-                    print(f)
+                    print(file)
         else:
-            for f in files:
-                df = pd.read_csv(f)
+            for file in files:
+                df = pd.read_csv(file)
                 [column_names.append(c) for c in (df.columns)]
 
             for i in sorted(set(column_names)):
@@ -61,12 +61,14 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "-d",
-        help="This flag allows you to pass a path to a traject config file in and a sorted list of fields in the provider data that are not in the config.",
+        help="""This flag allows you to pass a path to a traject config file in and a sorted list of fields
+        in the provider data that are not in the config.""",
     )
     parser.add_argument(
         "-r",
         action='store_true',
-        help="This flag is used to reverse the order of search. It returns a list of fields in the traject config that are not column names in the source data.",
+        help="""This flag is used to reverse the order of search. It returns a list of fields in the traject
+        config that are not column names in the source data.""",
     )
     args = parser.parse_args()
     main()
