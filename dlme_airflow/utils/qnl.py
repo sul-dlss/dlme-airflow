c0bd54a7175614c68c587fadb02dd92f067bccf6
Ed Summers <ehs@pobox.com>
2022-11-09 15:11:00 -0500

    simplified merge_df

--- dlme_airflow/utils/qnl.py
+++ dlme_airflow/utils/qnl.py
@@ -1,7 +1,8 @@
 # /bin/python
 import os
-import numpy as np
-import pandas as pd
+import pandas
+
+from itertools import chain
 
 
 def merge_records(**kwargs):
@@ -9,24 +10,14 @@ def merge_records(**kwargs):
     root_dir = os.path.dirname(os.path.abspath("metadata"))
     data_path = coll.data_path()
     working_csv = os.path.join(root_dir, "metadata", data_path, "data.csv")
-    df = pd.read_csv(working_csv)
+    df = pandas.read_csv(working_csv)
     df = merge_df(df)
     df.to_csv(working_csv)
 
 
-def merge_df(df) -> pd.DataFrame:
-    df_filled = df.fillna("NOT PROVIDED")
+def merge_df(df) -> pandas.DataFrame:
+    df_filled = df.fillna("")
     df_merged = df_filled.groupby("location_shelfLocator", as_index=False).agg(
-        lambda x: np.unique(x)
+        lambda x: list(chain(*x))
     )
     return df_merged
-
-
-def squash_lists(value):
-    value = [x.replace("\n", "") for x in value]
-    return (value[0] + value[1]).replace("][", ", ") if len(value) > 1 else value
-
-
-def squash_df(df) -> pd.DataFrame:
-    df_squashed = df.applymap(squash_lists)
-    return df_squashed
--- tests/utils/test_qnl.py
+++ tests/utils/test_qnl.py
@@ -1,60 +1,33 @@
 import pandas
-from ast import literal_eval
 
 from dlme_airflow.utils.qnl import merge_df
-from dlme_airflow.utils.qnl import squash_df
-from dlme_airflow.utils.qnl import squash_lists
 
 
-def test_merge():
-    df = pandas.read_csv("tests/data/csv/qnl.csv")
-    assert len(df) == 20
-    assert len(df.columns) == len(
-        merge_df(df).columns
-    ), "number of columns didn't change"
-
-    df = merge_df(df)
-    assert len(df) == 10
-    return df
-
-
-def test_squash_lists():
-    df = pandas.read_csv("tests/data/csv/qnl.csv")
-    df = merge_df(df)
-
-    # We sqaush multiple string values that look like lists into one.
-    # e.g. "['one'], ['two']" > "[one, two]"
-    # Then we check for lengths 1 and 2 in qnl.py:27.
-    # This test ensures that there are no other lenghts
-    for column in df:
-        if column != "location_shelfLocator":
-            for i in df[column].values:
-                assert len(i) <= 2
-    assert (
-        squash_lists(df.subject_topic[1])
-        == "['Medicine, Arab', 'Medicine, Medieval', 'Medicine, Greek and Roman', 'الطب عند العرب',"
-        " 'الطب في العصور الوسطى', 'طب، إغريقي وروماني']"
-    )
-    assert (
-        squash_lists(df.physicalDescription_extent[1])
-        == "['Material: PaperDimensions: 295 x 212 mm leaf [198 x 127 mm written]Foliation: British Museum"
-        " foliation in pencil; Arabic pagination in red inkRuling: Misṭarah; 19 lines per page; vertical spacing"
-        " 10 lines per 10 cm; the written area is enclosed by a frame of double lines in red inkScript: NaskhInk: "
-        "Black ink, with rubricated headings and overlinings in red; text frame in red inkBinding: British Museum "
-        "binding in dark brown leatherCondition: Excellent conditionMarginalia: None', 'المادة: ورقيةالأبعاد:"
-        " حجم الورقة ٢٩٥ × ٢١٢ مم /[المساحة المكتوبة ١٩٨ × ١٢٧ مم]ترقيم الأوراق: ترقيم "
-        "المتحف البريطاني باستخدام قلم رصاص؛ ترقيم الصفحات باللغة العربية بالحبر "
-        "الأحمرالتسطير: مسطرة؛ ١٩ سطرًا في كل صفحة؛ مسافة رأسية ١٠ سطور كل ١٠ سم؛ يحيط "
-        "بالمساحة المكتوبة إطار من سطرين مزدوجين بالحبر الأحمرالخط: نسخالحبر: حبر أسود مع "
-        "تحمير العناوين والخطوط الأفقية أعلى النص بالحبر الأحمر؛ إطار النص بالحبر "
-        "الأحمرالتجليد: تجليد المتحف البريطاني بالجلد البني الداكنالحالة: حالة ممتازة"
-        "الحواشي: لا يوجد']"
+def test_merge_df():
+    df = pandas.DataFrame(
+        [
+            # this row has three columns
+            {
+                "location_shelfLocator": "123",
+                "title": ["Hi!"],
+                "subject": ["Greetings"],
+            },
+            # this only has two
+            {"location_shelfLocator": "123", "title": ["Bonjour!"]},
+        ]
     )
 
+    # make sure the DataFrame looks how we expect
+    assert len(df) == 2
+    assert df.iloc[0].title == ["Hi!"]
+    assert df.iloc[1].title == ["Bonjour!"]
 
-def test_squash_df():
-    df = pandas.read_csv("tests/data/csv/qnl.csv")
+    # merge the rows
     df = merge_df(df)
-    df = squash_df(df)
-    assert len(literal_eval(df.subject_topic[1])) == 6
-    assert len(literal_eval(df.subject_name_namePart[1])) == 10
+
+    # make sure the rows got merged
+    assert len(df) == 1
+    row = df.iloc[0]
+    assert type(row.title) == list
+    assert set(row.title) == {"Hi!", "Bonjour!"}
+    assert set(row.subject) == {"Greetings"}
