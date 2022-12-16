3f6056550fc92a5120192c020026adeb176a7088
jacobthill <jacobhill.mail@gmail.com>
2022-12-16 16:01:02 -0500

    add new princeton collections and post-harvest task

---
+++ dlme_airflow/utils/princeton.py
@@ -0,0 +1,14 @@
+# /bin/python
+import os
+import pandas as pd
+
+def remove_ymdi(**kwargs):
+    coll = kwargs["collection"]
+    root_dir = os.path.dirname(os.path.abspath("metadata"))
+    data_path = coll.data_path()
+    working_csv = os.path.join(root_dir, "working", data_path, "data.csv")
+    df = pd.read_csv(working_csv)
+    # Filter out ymdi records and over write the csv
+    df = df[~df["member-of-collections"].str.contains('Yemeni Manuscript Digitization Initiative')]
+
+    df.to_csv(working_csv)
