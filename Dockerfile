cb6478cc8cd61e51c92c0be7a440d3a0f0f5e6dc
Aaron Collier <aaron.collier@stanford.edu>
2023-01-13 14:17:34 -0800

    Add docker proxy service for use by DockerOperator

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM apache/airflow:2.4.0-python3.10
+FROM apache/airflow:2.5.0-python3.10
 
 ENV POETRY_VERSION=1.2.0
 
--- dlme_airflow/tasks/transform.py
+++ dlme_airflow/tasks/transform.py
@@ -4,35 +4,21 @@ import os
 from airflow import DAG
 
 # Operators and utils required from airflow
-from airflow.providers.amazon.aws.operators.ecs import EcsOperator
+from airflow.providers.docker.operators.docker import DockerOperator
 from airflow.utils.task_group import TaskGroup
 
 
 def build_transform_task(collection, task_group: TaskGroup, dag: DAG):
-    return EcsOperator(
+    return DockerOperator(
         task_id=f"transform_{collection.label()}",
         task_group=task_group,
-        aws_conn_id="aws_conn",
-        cluster="dlme-dev",
-        task_definition="dlme-transform",
-        launch_type="FARGATE",
-        overrides={
-            "containerOverrides": [
-                {
-                    "name": "dlme-transform",
-                    "environment": [
-                        {"name": "DATA_PATH", "value": collection.data_path()},
-                    ],
-                },
-            ],
+        image='suldlss/dlme-transform:latest',
+        api_version='auto',
+        auto_remove=True,
+        environment={
+            "DATA_PATH": collection.data_path()
         },
-        network_configuration={
-            "awsvpcConfiguration": {
-                "securityGroups": [
-                    os.environ.get("SECURITY_GROUP_ID", os.getenv("ECS_SECURITY_GROUP"))
-                ],
-                "subnets": [os.environ.get("SUBNET_ID", os.getenv("ECS_SUBNET"))],
-            },
-        },
+        docker_url="tcp://docker-proxy:2375",
+        network_mode="bridge",
         dag=dag,
     )
