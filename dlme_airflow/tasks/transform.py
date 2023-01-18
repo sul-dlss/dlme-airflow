980061e15a145557970766d1a78fdd3a9e8e99c1
Aaron Collier <aaron.collier@stanford.edu>
2023-01-18 13:59:27 -0800

    Mount local storage into transform task, part 1

--- dlme_airflow/tasks/transform.py
+++ dlme_airflow/tasks/transform.py
@@ -6,13 +6,13 @@ from airflow import DAG
 # Operators and utils required from airflow
 from airflow.providers.docker.operators.docker import DockerOperator
 from airflow.utils.task_group import TaskGroup
-
+from docker.types import Mount
 
 def build_transform_task(collection, task_group: TaskGroup, dag: DAG):
     return DockerOperator(
         task_id=f"transform_{collection.label()}",
         task_group=task_group,
-        image='suldlss/dlme-transform:latest',
+        image='suldlss/dlme-transform:spike',
         api_version='auto',
         auto_remove=True,
         environment={
@@ -21,4 +21,11 @@ def build_transform_task(collection, task_group: TaskGroup, dag: DAG):
         docker_url="tcp://docker-proxy:2375",
         network_mode="bridge",
         dag=dag,
+        mounts=[
+            Mount(
+                source="/opt/airflow/metadata",
+                target="/opt/airflow/metadata:r",
+                type="bind"
+            )
+        ]
     )
