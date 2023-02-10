97122e595c2e109c7ad2b4bf42b69d0d8763b3fd
Aaron Collier <aaron.collier@stanford.edu>
2023-02-10 14:29:42 -0800

    User DockerOperator for transform task

--- dlme_airflow/tasks/transform.py
+++ dlme_airflow/tasks/transform.py
@@ -4,35 +4,36 @@ import os
 from airflow import DAG
 
 # Operators and utils required from airflow
-from airflow.providers.amazon.aws.operators.ecs import EcsOperator
+from airflow.providers.docker.operators.docker import DockerOperator
 from airflow.utils.task_group import TaskGroup
+from docker.types import Mount
 
 
 def build_transform_task(collection, task_group: TaskGroup, dag: DAG):
-    return EcsOperator(
+    return DockerOperator(
         task_id=f"transform_{collection.label()}",
         task_group=task_group,
-        aws_conn_id="aws_conn",
-        cluster="dlme-dev",
-        task_definition="dlme-transform",
-        launch_type="FARGATE",
-        overrides={
-            "containerOverrides": [
-                {
-                    "name": "dlme-transform",
-                    "environment": [
-                        {"name": "DATA_PATH", "value": collection.data_path()},
-                    ],
-                },
-            ],
+        image=os.environ.get("TRANSFORM_IMAGE"),
+        api_version="auto",
+        auto_remove=True,
+        environment={
+            "DATA_PATH": collection.data_path(),
+            "SOURCE_DATA": os.environ.get("SOURCE_DATA_PATH"),
         },
-        network_configuration={
-            "awsvpcConfiguration": {
-                "securityGroups": [
-                    os.environ.get("SECURITY_GROUP_ID", os.getenv("ECS_SECURITY_GROUP"))
-                ],
-                "subnets": [os.environ.get("SUBNET_ID", os.getenv("ECS_SUBNET"))],
-            },
-        },
+        docker_url="tcp://docker-proxy:2375",
+        network_mode="bridge",
         dag=dag,
+        mounts=[
+            Mount(
+                source=os.environ.get("METADATA_OUTPUT_PATH"),
+                target="/opt/airflow/metadata",
+                type="bind",
+            ),
+            Mount(
+                source=os.environ.get("SOURCE_DATA_PATH"),
+                target="/opt/airflow/working",
+                type="bind",
+            ),
+        ],
+        mount_tmp_dir=False,
     )
