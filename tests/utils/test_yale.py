394d9b4ebda3926cce4e0c6e7e35d21d7b44df45
Aaron Collier <aaron.collier@stanford.edu>
2023-02-13 15:39:53 -0800

    Mock write when testing to avoid overwriting fixture data

--- tests/utils/test_yale.py
+++ tests/utils/test_yale.py
@@ -1,8 +1,20 @@
+import pytest
+import pandas
+
 from dlme_airflow.utils.yale import get_working_csv, remove_babylonian_non_relevant
 from dlme_airflow.models.provider import Provider
 
 
-def test_remove_babylonian_non_relevant(mocker):
+# This mock prevents writing to the fixture CSV when testing
+@pytest.fixture
+def mock_dataframe_to_csv(monkeypatch):
+    def mock_to_csv(_self, _filename):
+        return True
+
+    monkeypatch.setattr(pandas.DataFrame, "to_csv", mock_to_csv)
+
+
+def test_remove_babylonian_non_relevant(mocker, mock_dataframe_to_csv):
     mocker.patch(
         "dlme_airflow.utils.yale.get_working_csv",
         return_value="tests/data/csv/yale.csv",
