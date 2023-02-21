4d145d4e215175ea80c7bcca0bb70fa23e1fa872
Aaron Collier <aaron.collier@stanford.edu>
2023-02-21 13:37:01 -0800

    Improve test coverage of utils/princeton

--- tests/utils/test_catalog.py
+++ tests/utils/test_catalog.py
@@ -1,4 +1,5 @@
 from dlme_airflow.utils.catalog import get_working_csv
 
+
 def test_get_working_csv():
     assert "metadata/test_collection/data.csv" in get_working_csv("test_collection")
--- tests/utils/test_princeton.py
+++ tests/utils/test_princeton.py
@@ -1,6 +1,28 @@
+import pytest
 import pandas
 
-from dlme_airflow.utils.princeton import filter_df
+from dlme_airflow.utils.princeton import filter_df, remove_ymdi
+from dlme_airflow.models.provider import Provider
+
+
+# This mock prevents writing to the fixture CSV when testing
+@pytest.fixture
+def mock_dataframe_to_csv(monkeypatch):
+    def mock_to_csv(_self, _filename):
+        return True
+
+    monkeypatch.setattr(pandas.DataFrame, "to_csv", mock_to_csv)
+
+
+def test_remove_ymdi(mocker, mock_dataframe_to_csv):
+    mocker.patch(
+        "dlme_airflow.utils.catalog.get_working_csv",
+        return_value="tests/data/csv/princeton.csv",
+    )
+    provider = Provider("princeton")
+    params = {"collection": provider.get_collection("islamic_manuscripts")}
+
+    assert "metadata/princeton/islamic_manuscripts/data.csv" in remove_ymdi(**params)
 
 
 def test_filter_df():
