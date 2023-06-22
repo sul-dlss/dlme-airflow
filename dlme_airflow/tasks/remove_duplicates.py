46651c311cfa16fe4a76c51d64f505561981b933
Aaron Collier <aaron.collier@stanford.edu>
2023-06-22 13:55:40 -0700

    Use collection model method for the datafile

--- dlme_airflow/tasks/remove_duplicates.py
+++ dlme_airflow/tasks/remove_duplicates.py
@@ -6,20 +6,21 @@ from airflow import DAG
 # Operators and utils required from airflow
 from airflow.operators.python import PythonOperator
 from airflow.utils.task_group import TaskGroup
-from dlme_airflow.utils.catalog import get_working_csv
+
 
 def run_remove_duplicates(**kwargs):
     coll = kwargs["collection"]
     data_path = coll.data_path()
-    working_csv = get_working_csv(data_path)
+    working_csv = coll.datafile("csv")
 
     if os.path.isfile(working_csv):
         df = pd.read_csv(working_csv)
         # Filter out duplicate records and over write the csv
-        df = df.drop_duplicates(subset="id")
+        df = df.drop_duplicates(subset="identifier")
         df.to_csv(working_csv)
 
     return working_csv
+
 
 def build_remove_duplicates_task(collection, task_group: TaskGroup, dag: DAG):
     return PythonOperator(
