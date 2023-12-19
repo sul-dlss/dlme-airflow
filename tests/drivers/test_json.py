b6e221dd3e46ca85d171d99b6547ec8dd3f90cda
Aaron Collier <aaron.collier@stanford.edu>
2023-12-19 08:39:00 -0800

    Add incremental paging json test case

--- tests/drivers/test_json.py
+++ tests/drivers/test_json.py
@@ -3,7 +3,7 @@ import json
 from dlme_airflow.drivers.json import JsonSource
 
 
-def test_happy_path(requests_mock):
+def test_happy_path_pages_from_provider(requests_mock):
     # mock the http call to return some real loc.gov data
     loc_data = json.load(open("tests/data/json/loc.json"))
     requests_mock.get(
@@ -60,4 +60,61 @@ def test_happy_path(requests_mock):
     assert (
         df.thumbnail[99]
         == "https://tile.loc.gov/image-services/iiif/service:amed:amedpllc:00406537456:0055/full/pct:6.25/0/default.jpg"
+    )
+
+
+def test_happy_path_incremental_paging(requests_mock):
+    # mock the http call to return some real loc.gov data
+    loc_data = json.load(open("tests/data/json/incremental_paging.json"))
+    requests_mock.get(
+        "https://example.com/",
+        json=loc_data,
+    )
+
+    # the collection selector json path defined in the Intake catalog
+    record_selector = "results"
+
+    # the collection url defined in the Intake catalog
+    collection_url = (
+        "https://example.com/"
+    )
+
+    # this jsonpath configuration is required in the intake catalog
+    metadata = {
+        "fields": {
+            "id": {"path": "accessionNumber"},
+            "title": {"path": "title"},
+            "thumbnail": {"path": "image"},
+        },
+    }
+
+    # create our JsonSource object
+    js = JsonSource(
+        collection_url,
+        record_selector=record_selector,
+        metadata=metadata,
+        paging={"increment": 20, "query_param": "offset", "result_count": "totalResults"},
+    )
+
+    # get the DataFrame
+    df = js.read()
+
+    assert len(df) == 20
+    assert len(df.columns) == 3
+    assert set(df.columns) == set(["id", "title", "thumbnail"])
+
+    assert df.id[0] == "23.3.935"
+    assert df.title[0] == "Wall casing"
+    assert (
+        df.thumbnail[0]
+        == "https://images.metmuseum.org/CRDImages/eg/mobile-large/bb468-70.jpg"
+    )
+
+    assert df.id[19] == "26.7.1183"
+    assert (
+        df.title[19] == "Kohl Tube in the Form of a Papyrus Column"
+    )
+    assert (
+        df.thumbnail[19]
+        == "https://images.metmuseum.org/CRDImages/eg/mobile-large/LC-26_7_1183_EGDP034542.jpg"
     )
