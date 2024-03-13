0a23f2a453c25ece24a54af0eb0c6e7353f0e134
Aaron Collier <aaron.collier@stanford.edu>
2024-03-12 17:19:29 -0700

    Split the mena_islamic_art collection out

--- tests/feature/test_walters.py
+++ tests/feature/test_walters.py
@@ -17,7 +17,7 @@ def mock_collection_datafile(monkeypatch, tmp_path):
     monkeypatch.setattr(Collection, "datafile", mock_datafile)
 
 
-def test_filter_walters_records(mocker, mock_collection_datafile, tmp_path):
+def test_filter_walters_mena(mocker, mock_collection_datafile, tmp_path):
     provider = Provider("walters")
     params = {"collection": provider.get_collection("mena")}
     source_dataframe = pandas.read_json("tests/data/json/walters/unfiltered.json")
@@ -27,4 +27,21 @@ def test_filter_walters_records(mocker, mock_collection_datafile, tmp_path):
     filtered_dataframe = pandas.read_json(f"{tmp_path}/walters.json")
     assert source_dataframe.shape[0] == 18
     assert filtered_dataframe.shape[0] == 15
+    # This test confirms that the ObjectIDs 11, 14, and 74 are not in the filtered dataframe
+    # ObjectID 11 because Culture = "Not Egyptian" which is not in the Culture filter list
+    # ObjectIDs 14 and 74 because Collection = "Islamic Art" which are not in this Collection filter list
     assert set(source_dataframe.ObjectID).difference(set(filtered_dataframe.ObjectID)) == {11, 14, 74}
+
+
+def test_filter_walters_mena_islamic_art(mocker, mock_collection_datafile, tmp_path):
+    provider = Provider("walters")
+    params = {"collection": provider.get_collection("mena_islamic_art")}
+    source_dataframe = pandas.read_json("tests/data/json/walters/unfiltered.json")
+
+    assert f"{tmp_path}/walters.json" in filter_dataframe(**params)
+    
+    filtered_dataframe = pandas.read_json(f"{tmp_path}/walters.json")
+    assert source_dataframe.shape[0] == 18
+    assert filtered_dataframe.shape[0] == 2
+    # This test confirms that the ObjectIDs 14, and 74 are in the Collection filter list
+    assert set(filtered_dataframe.ObjectID) == {14, 74}
